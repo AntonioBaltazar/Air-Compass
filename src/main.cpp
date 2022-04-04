2b5488b08d129e4b92eeead25113edc5d9970dd7
martindochamp <martinpaulcondet@gmail.com>
2022-04-04 19:22:44 +0200

    Menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <conio.h>
 #include "airplane.h"
 #include "airport.h"
 #include "graph.h"
@@ -10,17 +11,45 @@
 using namespace std;
 using namespace GraphReader;
 
-int main(int argc, char *argv[]) {
+void antonio() {
+    AerialNetwork test;
+    test.create_fleet();
+    for (int i = 0; i < 5; i++)
+        test.get_fleet()[i].display();
+}
+
+void martin() {
     Graph g;
     GraphReader::load_graph("graph.txt", g);
     AerialNetwork test;
-    
-    test.create_fleet();
-   
+}
 
-    for (int i = 0; i < 5; i++)
-        test.get_fleet()[i].display();
-    
-    
+void arnaud() {
+
+}
+
+int main(int argc, char *argv[]) {
+
+    while(1) {
+        while(!kbhit()) {
+            cout << "\n\n\nAirCompass : \n[1] Tonio\n[2] Martin\n[3] Arnaud\n[4] Quitter";
+            int c = getch();
+            switch(c) {
+                case 49:
+                    antonio();
+                    break;
+                case 50:
+                    martin();
+                    break;
+                case 51:
+                    arnaud();
+                    break;
+                case 52:
+                    return 0;
+                default: break;
+            }
+        }
+    }
+
     return 0;
 }
