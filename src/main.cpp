8efca57ba856669df1b811ff26f6db9149fa883d
martindochamp <martinpaulcondet@gmail.com>
2022-04-05 12:46:16 +0200

    Adapting lib to linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <conio.h>
 #include "airplane.h"
 #include "airport.h"
 #include "graph.h"
 #include "graphreader.h"
 #include "aerialnetwork.h"
 #include "travel.h"
+#include <ncurses.h>
 
 using namespace std;
 using namespace GraphReader;
@@ -31,23 +31,22 @@ void arnaud() {
 int main(int argc, char *argv[]) {
 
     while(1) {
-        while(!kbhit()) {
-            cout << "\n\n\nAirCompass : \n[1] Tonio\n[2] Martin\n[3] Arnaud\n[4] Quitter";
-            int c = getch();
-            switch(c) {
-                case 49:
-                    antonio();
-                    break;
-                case 50:
-                    martin();
-                    break;
-                case 51:
-                    arnaud();
-                    break;
-                case 52:
-                    return 0;
-                default: break;
-            }
+        cout << "\n\n\nAirCompass : \n[1] Tonio\n[2] Martin\n[3] Arnaud\n[4] Quitter";
+        int c = getc(stdin);
+
+        switch(c) {
+            case 49:
+                antonio();
+                break;
+            case 50:
+                martin();
+                break;
+            case 51:
+                arnaud();
+                break;
+            case 52:
+                return 0;
+            default: break;
         }
     }
 
