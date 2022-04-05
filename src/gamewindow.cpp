7882070e914d269edc199c84408bef71b6015d64
martindochamp <martinpaulcondet@gmail.com>
2022-04-05 20:42:06 +0200

    Multiple surfaces

--- src/gamewindow.cpp
+++ src/gamewindow.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "gamewindow.h"
 
 using namespace std;
+
+void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius);
 
 bool GameWindow::init() {
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
@@ -11,39 +14,52 @@ bool GameWindow::init() {
 	} 
 
     //Create window
-    setWindow(SDL_CreateWindow(getWindowName().c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, getWidth(), getHeight(), SDL_WINDOW_SHOWN ));
+    SDL_Window* win{ nullptr };
+    SDL_Renderer* rend{ nullptr };
+    SDL_CreateWindowAndRenderer(getWidth(), getHeight(), SDL_WINDOW_SHOWN, &win, &rend);
+    setRender(rend);
+    setWindow(win);
     if(getWindow() == NULL ) {
         printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         return false;
     }
     
-    //Get window surface
-    setSurface(SDL_GetWindowSurface(getWindow()));
     return true;
 }
 
-bool GameWindow::loadMedia(std::string _path_image) {
-    setImage(SDL_LoadBMP(_path_image.c_str()));
-    return getImage() != nullptr;
-}
-
 bool GameWindow::close() {
-    SDL_FreeSurface(getImage());
-    setImage(NULL);
     SDL_DestroyWindow(getWindow());
     setWindow(NULL);
     SDL_Quit();
 }
 
 void GameWindow::run(std::string _path_image) {
+     SDL_Surface* image = IMG_Load("rsc/map.bmp");
+     SDL_Surface* image2 = IMG_Load("rsc/mario.bmp");
     if (!init()) printf("Failed to initialize!\n");
     else {
-        if (!loadMedia(_path_image)) printf("Failed to load media!\n");
-        else {
-            SDL_BlitSurface(getImage(), NULL, getSurface(), NULL);
-            SDL_UpdateWindowSurface(getWindow());
-        }
+        //SDL_BlitSurface(image, NULL, getSurface(), NULL);
+        SDL_UpdateWindowSurface(getWindow());
     }
+    
+    Ressource rsc1("rsc/map.bmp", 2*480, 480, 0, 0);
+    Ressource rsc2("rsc/mario.bmp", 2*480, 480, 0, 0);
+    SDL_Rect src1{0, 0, 0, 0};
+    SDL_Rect dst1{ 0, 0, 400, 300 };
+    SDL_Rect src2{ 0, 0, 0, 0 };
+    SDL_Rect dst2{ getWidth()- 240, getHeight() - 100, 240, 100};
+    src2.x = src1.w / 2 - 30;
+    src2.y = src1.h / 2 - 50;
+    src2.w = 50;
+    src2.h = 50;
+
+   
+    SDL_Texture* t1 = SDL_CreateTextureFromSurface(getRender(), image);
+    SDL_Texture* t2 = SDL_CreateTextureFromSurface(getRender(), image2);
+    SDL_FreeSurface(image);
+
+    SDL_QueryTexture(t1, nullptr, nullptr, &src1.w, &src1.h);
+    //SDL_QueryTexture(t1, nullptr, nullptr, &src1.w, &src1.h);
 
     SDL_Event events;
     bool isOpen{true};
@@ -56,6 +72,51 @@ void GameWindow::run(std::string _path_image) {
                 default: break;
             }
         }
+        SDL_SetRenderDrawColor(getRender(), 0, 0, 0, 255); 
+        SDL_RenderClear(getRender());
+
+        SDL_RenderCopy(getRender(), t1, NULL, NULL);
+        SDL_RenderCopy(getRender(), t2, NULL, &dst2);
+
+        SDL_RenderPresent(getRender());  
+ 
     }
     close();
+}
+
+void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius) { 
+    const int32_t diameter = (radius * 2);
+
+   int32_t x = (radius - 1);
+   int32_t y = 0;
+   int32_t tx = 1;
+   int32_t ty = 1;
+   int32_t error = (tx - diameter);
+
+   while (x >= y)
+   {
+      //  Each of the following renders an octant of the circle
+      SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+      SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+      SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+      SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+      SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+      SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+      SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+      SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+      if (error <= 0)
+      {
+         ++y;
+         error += ty;
+         ty += 2;
+      }
+
+      if (error > 0)
+      {
+         --x;
+         tx += 2;
+         error += (tx - diameter);
+      }
+   }
 }
--- src/gamewindow.h
+++ src/gamewindow.h
@@ -2,14 +2,33 @@
 #define GAMEWINDOW_H_INCLUDED
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <vector>
+
+class Ressource {
+    private: 
+        SDL_Surface* m_ressource = NULL;
+        int m_width, m_height;
+        int m_init_x, m_init_y;
+        std::string m_path;
+    public: 
+        Ressource(std::string _path, int _width, int _height, int _x, int _y) : m_path(_path), m_width(_width), m_height(_height), m_init_x(_x), m_init_y(_y) {
+            m_ressource = IMG_Load(m_path.c_str());
+        }
+        ~Ressource() {}
+        SDL_Surface* getSurface() { return m_ressource; }
+  
+};
 
 class GameWindow {
     private:
         std::string m_window_name;
         int m_screen_width, m_screen_height;
         SDL_Window* m_window = NULL;
+        SDL_Renderer* m_render = NULL;
         SDL_Surface* m_surface = NULL;
-        SDL_Surface* m_image = NULL;
+        std::vector<Ressource> m_ressources;
+
     public:
         // Constructors & Destructor
         GameWindow(std::string _window_name, int _screen_width, int _screen_height) : 
@@ -18,23 +37,26 @@ class GameWindow {
 
         // Getters
         SDL_Window* getWindow() const { return m_window; }
-        SDL_Surface* getSurface() const { return m_surface; }
-        SDL_Surface* getImage() const { return m_image; }
+        SDL_Renderer* getRender() const { return m_render; }
         int getWidth() const { return m_screen_width; }
         int getHeight() const { return m_screen_height; }
+        SDL_Surface* getSurface() const { return m_surface; }
         std::string getWindowName() const { return  m_window_name; }
 
         // Setters
         void setWindow(SDL_Window* _window) { m_window = _window; }
+ 
+        void setRender(SDL_Renderer* _render) { m_render = _render;}
         void setSurface(SDL_Surface* _surface) { m_surface = _surface; }
-        void setImage(SDL_Surface* _image) { m_image = _image; }
 
         // Methods
         bool init();
         bool loadMedia(std::string _path_image);
         bool close();
         void run(std::string _path_image);
-        SDL_Surface* resizeImage(int w,int h,SDL_Surface* src);
+
+        void addRessource(Ressource _rsc) { m_ressources.push_back(_rsc); }
+   
 };
 
 #endif // GAMEWINDOW_H_INCLUDED
