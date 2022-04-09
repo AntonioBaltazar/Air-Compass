77b15e28776f782a9cd9e765ad35dd4b30058694
martindochamp <martinpaulcondet@gmail.com>
2022-04-10 00:39:20 +0200

    Divergence

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <random>
 #include "graph.h"
 #include "simulation.h"
 #include "gamewindow.h"
 
 using namespace std;
 
+
+
 void Flight::next_step() { m_tick++; }
 
 void Simulation::assign_flight() {
+    
+
     Airplane r_airplane = m_aerial_network->get_fleet()[5];
     //m_aerial_network->get_fleet()[rand() % m_aerial_network->get_fleet().size()];
     Graph temp = *m_graph;
     temp.limitEdge(r_airplane.get_autonomy());
    // m_graph->display_graph();
     vector<Edge> edges = temp.get_edges();
-    Edge r_edge = edges[rand() % edges.size()];
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, edges.size() - 1); // distribution in range [1, 6]
+    int r = dist6(rng);
+    Edge r_edge = edges[r];
+    SDL_Log("%d \n", r);
+    
     //SDL_Log(string("DEBUG"+edges.size()).c_str());
+
     m_flights.push_back(Flight(r_airplane, r_edge));
 }
 
 void Simulation::next_step() {
+    
+    int count = 0;
     for (auto i = m_flights.begin(); i != m_flights.end(); ++i) {
+        if ((*i).get_tick() >= (*i).get_edge().weigth) {
+            //SDL_Log("FINISHED");
+            m_flights.erase(i--);
+            int t = (*i).get_edge().src + 1;
+            
+            assign_flight();
+        } else {
+            (*i).next_step();
+            //(*i).display();
+        }/* 
         if ((*i).get_tick() <= (*i).get_edge().weigth) {
             if ((*i).get_tick() == (*i).get_edge().weigth) {
                 SDL_Log("DEBUG");
@@ -35,7 +59,8 @@ void Simulation::next_step() {
             SDL_Log("DEBUG");
             m_flights.erase(i--);
             assign_flight();
-        } 
+        }  */
+        count++;
     }
 }
 
